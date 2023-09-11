d36c9ab94f04c537658edc2f275a2e40f661b14b
P.John <100846319+mr-PJohn@users.noreply.github.com>
2023-09-11 16:57:32 +0900

    Create Simulation_II.py

---
+++ Simulation_II.py
@@ -0,0 +1,20 @@
+import random
+from time import *
+# VAR set zone
+b = 1
+def checking(a):
+    try:
+        if a == 0:
+            for i in range(10000000):
+                sleep(0.0001)
+                ran = [random.randrange(0, 2) for i in range(10)]
+                print(ran)
+                if ran == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
+                    how
+                elif ran == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
+                    how
+    finally:
+        print("어케 했노???????")
+if __name__=='__main__':
+  checking(0)
+
